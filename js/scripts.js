95672c03715f585f0cb3864f933a376e5c525a01
Izabella Ribeiro <izabella.ribeirro@gmail.com>
2020-10-26 17:07:41 -0300

    add variables

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// Declaração de variáveis 
+const question = document.querySelector("#question");
+const answerBox = document.querySelector("#answers-box");
+const quizzContainer = document.querySelector("#quizz-container");
+const scoreContainer = document.querySelector("#score-container");
